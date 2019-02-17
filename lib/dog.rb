7b5b2f6c8e4f66c709271062422136ac0b200ffe
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-02-17 03:34:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -29,7 +29,7 @@ class Dog
   end
 
   def self.find_by_id(id)
-    row = DB[:conn].execute("SELECT * FROM dogs WHERE id = ?", id).last
+    row = DB[:conn].execute("SELECT * FROM dogs WHERE id = ?", id)[0]
     dog_attr = {:id => row[0], :name => row[1], :breed => row[2]}
     dog = self.create(dog_attr)
     binding.pry
